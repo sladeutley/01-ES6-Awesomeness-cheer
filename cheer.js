4864c15c5bf68779f8891bf69500364fa2e08e80
Slade Utley <slade.utley@gmail.com>
2018-03-01 15:18:27 -0600

    completed exercise

--- cheer.js
+++ cheer.js
@@ -1,20 +1,3 @@
-// const firstLetter = "S";
-// process.stdout.write(`Give me an ${firstLetter}!\n`);
-
-// let secondLetter = "l";
-// process.stdout.write(`Give me an ${secondLetter}!\n`);
-
-// let getThirdLetter = (string) => string;
-// const letterObj = {
-//     letter: getThirdLetter
-// }
-// process.stdout.write(`Give me an ${letterObj.letter("a")}!\n`);
-
-// const newLetterObj = { getThirdLetter };
-// process.stdout.write(`Give me a ${newLetterObj.getThirdLetter("d")}!\n`);
-
-// process.stdout.write(`${firstLetter}${secondLetter}${letterObj.letter("a")}${newLetterObj.getThirdLetter("d")}!\n`);
-
 const myFirstName = process.argv[2];
 const myLastName = process.argv[3];
 
